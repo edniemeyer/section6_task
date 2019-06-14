a0caafadb1eacc90817aaa21ccfa49d0c76ed88b
eduardo <edniemeyer@gmail.com>
2019-06-14 19:27:53 +1200

    Added timezone info

--- dist/reader.js
+++ dist/reader.js
@@ -1,14 +1,16 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+const moment = require('moment-timezone');
 let data = [];
 fs.createReadStream("./src/timezone.csv")
     .pipe(csv())
     .on('data', (row) => {
-    console.log(row);
+    console.log(row.timestamp_utc);
+    console.log(moment().tz(Number(row.timestamp_utc)).format("DD/MM/YYYY hh:mm z ZZ").toString());
     data.push(row);
 })
     .on('end', () => {
     console.log('CSV file successfully processed');
-    console.log(data);
+    // console.log(data);
 });
 //# sourceMappingURL=reader.js.map
